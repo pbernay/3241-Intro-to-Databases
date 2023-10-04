587f0b8b35a7d1e8291c81d0aa73d1391e82f1c3
Paul Bernay <bernay.3@osu.edu>
2023-10-04 11:55:55 -0400

    Initial Add with base code

---
+++ src/App.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Clear the screen 
+            clearScreen();
+
+            // Display the main menu.
+            System.out.println("MAIN MENU");
+            System.out.println("------------------");
+            System.out.println("1. Search/Modify");
+            System.out.println("2. Add");
+            System.out.println("3. Exit");
+            System.out.print("Pick an Option (1,2): ");
+
+            // Read user input.
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    SearchModify(scanner);
+                    break;
+                case 2:
+                    System.out.println("Add selected.");
+                    break;
+                case 3:
+                    // Exit the program.
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            // Wait for user to press Enter to continue.
+            System.out.print("Press Enter to continue...");
+            scanner.nextLine(); // Consume the newline character.
+            scanner.nextLine(); // Wait for Enter.
+        }
+    }
+
+    public static void SearchModify(Scanner scanner) {
+        clearScreen();
+        System.out.println("Search/Modify selected.");
+        System.out.println("Availible entities to search in include:");
+        System.out.println("'Employee', 'Local Warehouse', 'Drone', 'Order', 'Equipment', 'Community Member', 'Payment', 'Reviews'");
+        // ^^ add list of entities to make it better - possibly pull it from the database - same with the attributes
+        System.out.println("------------------------------------------------------------------------------------------------------");
+        
+        System.out.println("What entity do you want to select to search in?");
+        String entityParam = scanner.nextLine();
+
+        System.out.println("What attribute would you want to search for?");
+        String attribute = scanner.nextLine();
+        System.out.print("Enter " + attribute + ": ");
+        String attributeParam = scanner.nextLine();
+        
+
+    }
+
+    private static void clearScreen() { //only works for windows I believe
+        try {
+            // Execute the "cls" command to clear the Windows console.
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.err.println("Error clearing console: " + e.getMessage());
+        }
+    }
+}
